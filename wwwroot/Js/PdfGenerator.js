29faa3d963b5037f12221ce919491bf4d6333b7f
johan pacheco <johanpach9@gmail.com>
2025-11-25 19:05:31 -0500

    '20251125-PrimerPush'

---
+++ wwwroot/Js/PdfGenerator.js
@@ -0,0 +1,9 @@
+﻿function saveAsFile(filename, bytesBase64) {
+    console.log("Llamando SaveAsFile");
+    var link = document.createElement('a');
+    link.download = filename;
+    link.href = "data:application/octet-stream;base64," + bytesBase64;
+    document.body.appendChild(link);  // Corregido: 'document.body' en lugar de 'document'
+    link.click();
+    document.body.removeChild(link);
+}
